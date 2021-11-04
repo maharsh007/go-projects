c93775fd9f183b6f39ca7debe5c882db6877db2b
Maharshi <Maharshi.Choudhury@pepsico.com>
2021-11-04 11:12:01 +0530

    Adding test for additional arguments

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
+	got := Hello("Chris")
 
-	want := "Hello, world"
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
